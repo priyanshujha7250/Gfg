95fc6b8a695c23ec53d5e4f4e86234f185e4a507
priyanshu7250 <priyanshujha7250@gmail.com>
2026-03-08 00:57:20 +0530

    AreaOfTheCircle

---
+++ oops/areaOfCircle.java
@@ -0,0 +1,26 @@
+package Gfg.oops;
+class circle{
+    double radius ;
+
+    double area(){
+        return Math.PI*radius*radius;
+    }
+    double perimeter(){
+        return 2*Math.PI*radius;
+    }
+    double circumference(){
+        return perimeter();
+    }
+}
+
+public class areaOfCircle {
+    public static void main(String[] args) {
+        circle c = new circle();
+        c.radius = 5;
+        System.out.println("Area of circle : "+c.area());
+        System.out.println("Perimeter of circle : " + c.perimeter());
+        System.out.println("Circumference of the circle : " + c.circumference());
+
+    }
+    
+}
