805e60cc12dad4a6234cf2dc409e45b1c0bb9866
priyanshu7250 <priyanshujha7250@gmail.com>
2026-03-08 01:10:35 +0530

    Area and perimeter og the rectangle

---
+++ oops/areaOfRectangle.java
@@ -0,0 +1,20 @@
+package Gfg.oops;
+class rectangle{
+    double length;
+    double breadth;
+    double area(){
+        return length*breadth;
+    }
+    double perimeter(){
+        return 2*(length+breadth);
+    }
+}
+public class areaOfRectangle {
+    public static void main(String[] args) {
+        rectangle r = new rectangle();
+        r.length = 10;
+        r.breadth= 12;
+        System.out.println("Area of the rectangle : " + r.area());
+        System.out.println("Perimeter of rectangle : " + r.perimeter());
+    }
+}
